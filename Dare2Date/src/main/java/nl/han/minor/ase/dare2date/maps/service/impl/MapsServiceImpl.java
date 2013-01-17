cd4da81af2a0d518a513c35b7825f98aa3b24ca8
kaaz <kaaz@145.74.158.229>
2013-01-17 15:42:16 +0100

    locationservice->avg plaats (werkt)

--- Dare2Date/src/main/java/nl/han/minor/ase/dare2date/maps/service/impl/MapsServiceImpl.java
+++ Dare2Date/src/main/java/nl/han/minor/ase/dare2date/maps/service/impl/MapsServiceImpl.java
@@ -4,6 +4,10 @@
  */
 package nl.han.minor.ase.dare2date.maps.service.impl;
 
+import java.net.URI;
+import java.net.URL;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import nl.han.minor.ase.dare2date.maps.MapLocation;
 import nl.han.minor.ase.dare2date.maps.service.MapsService;
 import nl.han.minor.ase.dare2date.profileservice.ProfileService;
@@ -25,17 +29,71 @@ public class MapsServiceImpl implements MapsService {
     private static final String NOKIA_APP_ID = "4FK7DX8Tl9fQS6czRnUw";
     private static final String NOKIA_TOKEN = "KBYlzoAUfx9hVRsedNC1rQ";
     private static final String NOKIA_MAPS_URL = "http://demo.places.nlp.nokia.com/discover/explore?app_id=" + NOKIA_APP_ID + "&app_code=" + NOKIA_TOKEN;
+    private static final String YAHOO_URL = "http://where.yahooapis.com/geocode?locale=en_US&flags=&offset=15&gflags=R&start=0&count=100&q=";
 
     @Override
     public MapLocation getLocation(int userId, int inviteeId) {
-
-        MapLocation userIdAddress = profileService.getAddress(userId);
-        MapLocation inviteeAddress = profileService.getAddress(inviteeId);
+        double latitude,longitude,lat1,lat2,long1,long2;
+        MapLocation user = profileService.getAddress(userId);
+        MapLocation invitee = profileService.getAddress(inviteeId);
+        org.w3c.dom.Document d = getYahooLocation(user);
+        
+        lat1 = Double.parseDouble( d.getElementsByTagName("latitude").item(0).getTextContent());
+        long1 = Double.parseDouble( d.getElementsByTagName("longitude").item(0).getTextContent());
+        
+        d = getYahooLocation(invitee);
+        
+        lat2 = Double.parseDouble( d.getElementsByTagName("latitude").item(0).getTextContent());
+        long2 = Double.parseDouble( d.getElementsByTagName("longitude").item(0).getTextContent());
+        
+        latitude = (lat1+lat2)/2;
+        longitude = (long1+long2)/2;
+        
+        d = getYahooLocation(latitude, longitude);
+        String result = d.getElementsByTagName("city").item(0).getTextContent();
         
         // TODO: find usable address based on these two.
         
-        MapLocation location = new MapLocation(UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN, UNKNOWN);
+        MapLocation location = new MapLocation(result, UNKNOWN, UNKNOWN, ""+latitude , ""+longitude);
         
         return location;
+    }
+    public org.w3c.dom.Document getYahooLocation(MapLocation m){
+        try{
+            URI uri = new URI(
+                    "http",
+                    "where.yahooapis.com",
+                    "/geocode?locale=en_US&flags=&offset=15&gflags=R&start=0&count=100&q="+m.getStreet()+" "+m.getNumber()+" "+m.getTown(),
+                    null
+                    );
+            String url = uri.toString();
+            log.debug("REQUESTURL\n"+url);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(url);
+        }
+        catch(Exception e){
+            
+        }
+        return null;
+    }
+    public org.w3c.dom.Document getYahooLocation(double latitude, double longitude){
+        try{
+            URI uri = new URI(
+                    "http",
+                    "where.yahooapis.com",
+                    "/geocode?locale=en_US&flags=&offset=15&gflags=R&start=0&count=100&q="+latitude+", "+longitude,
+                    null
+                    );
+            String url = uri.toString();
+            log.debug("REQUESTURL\n"+url);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(url);
+        }
+        catch(Exception e){
+            
+        }
+        return null;
     }
 }
